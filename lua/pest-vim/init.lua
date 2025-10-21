e0e9c67002a4e84daad8479d2d123a1d3233f692
Ulises Alanis <ulises.alanis.255@gmail.com>
2025-10-21 10:50:22 -0600

    require('lspconfig')._.setup{} is deprecated

--- lua/pest-vim/init.lua
+++ lua/pest-vim/init.lua
@@ -3,7 +3,9 @@ local M = {}
 function M.setup(opts)
     -- does not add anything on top of lspconfig currently.
     -- this may change in the future.
-    require("lspconfig").pest_ls.setup(opts)
+    --vim.lsp.config["pest_ls"]
+    vim.lsp.config['pest_ls'] = opts
+    vim.lsp.enable('pest_ls')
 end
 
 return M
